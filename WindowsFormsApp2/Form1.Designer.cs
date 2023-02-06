540d7b4b9225e93f59b6d34fb7bfa6fdd1a83e19
Пользователь <ilkinzeynalov010@gmail.com>
2023-02-06 16:42:31 +0400

    Init

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(115, 29);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(588, 39);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Aquamarine;
+            this.button1.Font = new System.Drawing.Font("MV Boli", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(306, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add Task";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(115, 205);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(588, 204);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(812, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Todo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string path = Directory.GetCurrentDirectory() + @"\..\..\" + "Data.txt";
+            FileStream fileStream = null;
+            try
+            {
+                fileStream = new FileStream(path, FileMode.Open);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Melumatlar silinib!", "System", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            
+            StreamReader reader = new StreamReader(fileStream);
+            while(reader.Peek() > 0)
+            {
+                listBox1.Items.Add(reader.ReadLine());
+            }
+            
+            reader.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(!(String.IsNullOrEmpty(richTextBox1.Text) || String.IsNullOrWhiteSpace(richTextBox1.Text)))
+            {
+                listBox1.Items.Add(richTextBox1.Text);
+                string path = Directory.GetCurrentDirectory() + @"\..\..\"  + "Data.txt";
+                FileStream fileStream = null;
+                if(File.Exists(path))
+                    fileStream = new FileStream(path, FileMode.Append);
+                else
+                {
+                    MessageBox.Show("Melumatlar silinib!", "System", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                StreamWriter  writer = new StreamWriter(fileStream);
+                writer.WriteLine(richTextBox1.Text);
+                writer.Close();
+            }
+            else
+            {
+                MessageBox.Show("Task bos ola bilmez!", "System", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            richTextBox1.Text = String.Empty;
+        }
+    }
+}
